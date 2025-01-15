0e5411e82031af77b238625c38db19a514c4829e
Shawn Leberknight <shawn.leberknight@gmail.com>
2025-01-15 16:12:22 -0500

    manage state

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,73 @@
-import { parseArgs } from "https://deno.land/std@0.218.0/cli/parse_args.ts";
-import type { ParseOptions } from "https://deno.land/std@0.218.0/cli/parse_args.ts";
+import { parseArgs } from 'https://deno.land/std@0.218.0/cli/parse_args.ts'
+import type { ParseOptions } from 'https://deno.land/std@0.218.0/cli/parse_args.ts'
 
 function parseArguments(args: string[]) {
   const config: ParseOptions = {
     alias: {
-      "h": "help",
-      "s": "save",
-      "n": "name",
-      "c": "color",
+      'h': 'help',
+      's': 'save',
+      'n': 'name',
+      'c': 'color',
     },
-    boolean: ["help", "save"],
-    string: ["name", "color"],
-  };
+    boolean: ['help', 'save'],
+    string: ['name', 'color'],
+  }
 
-  return parseArgs(args, config);
+  return parseArgs(args, config)
 }
 
 function printHelp(): void {
-  console.log(`Usage: greetme [OPTIONS...]`);
-  console.log("\nOptional flags:");
-  console.log("  -h, --help                Display this help and exit");
-  console.log("  -s, --save                Save settings for future greetings");
-  console.log("  -n, --name                Set your name for the greeting");
-  console.log("  -c, --color               Set the color of the greeting");
+  console.log(`Usage: greetme [OPTIONS...]`)
+  console.log('\nOptional flags:')
+  console.log('  -h, --help                Display this help and exit')
+  console.log('  -s, --save                Save settings for future greetings')
+  console.log('  -n, --name                Set your name for the greeting')
+  console.log('  -c, --color               Set the color of the greeting')
 }
 
 const greetings = [
-  "Hello",
-  "Hi",
-  "Hey",
-  "Good day",
-  "Good morning",
-  "Good evening",
-];
-function main(inputArgs: string[]): void {
-  const args = parseArguments(inputArgs);
+  'Hello',
+  'Hi',
+  'Hey',
+  'Good day',
+  'Good morning',
+  'Good evening',
+]
+
+async function main(inputArgs: string[]): Promise<void> {
+  const args = parseArguments(inputArgs)
 
   // If help flag enabled, print help.
   if (args.help) {
-    printHelp();
-    Deno.exit(0);
+    printHelp()
+    Deno.exit(0)
   }
 
-  const name: string | null = args.name;
-  const color: string | null = args.color;
-  const save: boolean = args.save;
+  let name: string | null = args.name
+  let color: string | null = args.color
+  const save: boolean = args.save
+
+  // Open KV key-value data store
+  const kv = await Deno.openKv('/tmp/kv.db')
+  // let askToSave = false;
+
+  if (!name) {
+    name = (await kv.get(['name'])).value as string
+  }
+  if (!color) {
+    color = (await kv.get(['color'])).value as string
+  }
+  if (save) {
+    await kv.set(['name'], name)
+    await kv.set(['color'], color)
+  }
 
   console.log(
     `%c${
       greetings[Math.floor(Math.random() * greetings.length) - 1]
     }, ${name}!`,
     `color: ${color}; font-weight: bold`,
-  );
+  )
 }
 
-main(Deno.args);
+main(Deno.args)
