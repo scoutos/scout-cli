993fe4fb51dab2cc7fdfa03a38a3b2570b9ce504
Cameron Monaghan <monaghancameron@gmail.com>
2025-02-05 10:27:22 -0500

    leave my test file allone

--- mod_test.ts
+++ mod_test.ts
@@ -1,10 +1,5 @@
-import { assertEquals } from 'https://deno.land/std@0.220.1/assert/mod.ts'
-import {
-  assertSpyCalls,
-  spy,
-} from 'https://deno.land/std@0.220.1/testing/mock.ts'
-import { join } from 'https://deno.land/std@0.218.0/path/mod.ts'
-import { config, scoutCli } from './mod.ts'
+// deno-lint-ignore ban-ts-comment
+// @ts-nocheck
 
 // Mock console.log and prompt
 const originalConsoleLog = console.log
